a552dd9d2a13d32fe52eef1b152597c8ffb925d7
Ryuji Watanabe <watanabe.ryuji717@mail.kyutech.jp>
2021-03-08 17:46:33 +0900

    Adapt callback function in base_gmm_net to show own team performance bars

--- gmm_net/models/base_gmm_net.py
+++ gmm_net/models/base_gmm_net.py
@@ -467,39 +467,40 @@ class BaseGMMNetworkOwnOppPerformance():
                     return self.get_return_list(**dict_update)
                 elif clicked_id_text == self.own_ukr_kde.ls.graph_whole.id:
                     # Update own team map to show clicked point
-                    ret_own_ukr_kde_fig_ls = self.own_ukr_kde.update_ls(
+                    fig_ls_own_ukr_kde = self.own_ukr_kde.update_ls(
                         clickData=clickData_own_tm,
                         prev_ls_fig_json=prev_own_tm_json
                     )
+                    fig_ls_opp_ukr_kde = go.Figure(**prev_opp_tm_json)
                     # Update own member map and dropdown to show density map
                     ret_own_lower_ukr_fig_ls, ret_own_lower_ukr_dropdown = self.own_ukr_kde.update_fs_dropdown_from_ls(
                         clickData=clickData_own_tm,
                         prev_fs_fig_json=prev_own_mm_json
                     )
                     # Update opp team map to show conditional or marginal component plane
-                    ret_opp_ukr_kde_fig_ls = self.own_opp_gplvm.update_ls(
+                    fig_ls_opp_ukr_kde = self.own_opp_gplvm.update_ls(
                         index_selected_feature=index_own_performance_opp_tm,
-                        fig_own_ls=ret_own_ukr_kde_fig_ls,
-                        fig_opp_ls=go.Figure(**prev_opp_tm_json),
+                        fig_own_ls=fig_ls_own_ukr_kde,
+                        fig_opp_ls=fig_ls_opp_ukr_kde,
                         which_update='opp'
                     )
                     dict_update = {
                         self.own_lower_ukr.ls.store_fig_whole.id: ret_own_lower_ukr_fig_ls,
                         self.own_lower_ukr.ls.dropdown.id: ret_own_lower_ukr_dropdown,
-                        self.own_ukr_kde.ls.store_fig_whole.id: ret_own_ukr_kde_fig_ls,
-                        self.opp_ukr_kde.ls.store_fig_whole.id: ret_opp_ukr_kde_fig_ls
+                        self.own_ukr_kde.ls.store_fig_whole.id: fig_ls_own_ukr_kde,
+                        self.opp_ukr_kde.ls.store_fig_whole.id: fig_ls_opp_ukr_kde,
                         # Why is this updated??? I can't understand my self
                         # self.own_opp_gplvm.dic_ls['own'].graph_whole.id: self.own_opp_gplvm.update_ls(
                         #     index_selected_feature=index_own_performance_own_tm,
                         #     clickData=clickData_opp_tm,
                         #     which_update='own'
                         # ),
-                        # ittan comment out
                         # Update own team performance bars
-                        # self.own_opp_gplvm.os.graph_indiv.id: self.own_opp_gplvm.update_bar(
-                        #     own_clickData=clickData_own_tm,
-                        #     opp_clickData=clickData_opp_tm
-                        # )
+                        self.own_opp_gplvm.os.store_fig_indiv.id: self.own_opp_gplvm.update_bar(
+                            fig_own_ls=fig_ls_own_ukr_kde,
+                            fig_opp_ls=fig_ls_opp_ukr_kde,
+                            prev_fig_bar_json=prev_own_tpb_json
+                        )
                     }
                     return self.get_return_list(**dict_update)
                 elif clicked_id_text == self.own_opp_gplvm.dic_ls['opp'].dropdown.id:
@@ -513,27 +514,33 @@ class BaseGMMNetworkOwnOppPerformance():
                     }
                     return self.get_return_list(**dict_update)
                 elif clicked_id_text == self.own_opp_gplvm.dic_ls['opp'].graph_whole.id:
-                    ret_opp_ukr_kde_fig_ls = self.opp_ukr_kde.update_ls(
+                    # Update opp team map to show clicked point
+                    fig_ls_opp_ukr_kde = self.opp_ukr_kde.update_ls(
                         clickData=clickData_opp_tm,
                         prev_ls_fig_json=prev_opp_tm_json
                     )
+                    fig_ls_own_ukr_kde = go.Figure(**prev_own_tm_json)
+                    # Update opp member map to show density
                     ret_opp_lower_ukr_fig_ls, temp = self.opp_ukr_kde.update_fs_dropdown_from_ls(
                         clickData=clickData_opp_tm,
                         prev_fs_fig_json=prev_opp_mm_json
                     )
                     dict_update = {
-                        self.opp_ukr_kde.ls.store_fig_whole.id: ret_opp_ukr_kde_fig_ls,
+                        self.opp_ukr_kde.ls.store_fig_whole.id: fig_ls_opp_ukr_kde,
                         self.opp_lower_ukr.ls.store_fig_whole.id: ret_opp_lower_ukr_fig_ls,
+                        # Update own team map to show component plane
                         self.own_ukr_kde.ls.store_fig_whole.id: self.own_opp_gplvm.update_ls(
                             index_selected_feature=index_own_performance_opp_tm,
-                            fig_own_ls=go.Figure(**prev_own_tm_json),
-                            fig_opp_ls=ret_opp_ukr_kde_fig_ls,
+                            fig_own_ls=fig_ls_own_ukr_kde,
+                            fig_opp_ls=fig_ls_opp_ukr_kde,
                             which_update='own'
+                        ),
+                        # Update own team performance bars
+                        self.own_opp_gplvm.os.store_fig_indiv.id: self.own_opp_gplvm.update_bar(
+                            fig_own_ls=fig_ls_own_ukr_kde,
+                            fig_opp_ls=fig_ls_opp_ukr_kde,
+                            prev_fig_bar_json=prev_own_tpb_json
                         )
-                        # self.own_opp_gplvm.os.graph_indiv.id: self.own_opp_gplvm.update_bar(
-                        #     own_clickData=clickData_own_tm,
-                        #     opp_clickData=clickData_opp_tm
-                        # ),
                     }
                     return self.get_return_list(**dict_update)
                 else:
